78990139960a9f54ec6d1cfdbfbc7d6af5f8a44b
Vladimir Boris <vladimir.boris.bel@gmail.com>
2018-05-21 20:24:33 -0600

    Migration to bootstrap 4. Footer layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component,
          OnInit} from '@angular/core';
 import { } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
     public phoneNumber;
-    constructor() {
+    constructor(private router: Router) {
         this.phoneNumber = '+2243341624';
     }
 
